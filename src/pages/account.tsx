190786f636bf99a13eba2d1d4124767a12492fc1
BreMea <63671187+BreMea@users.noreply.github.com>
2022-05-07 22:02:09 -0400

    feat: nav on login page

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -1,8 +1,10 @@
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import * as React from 'react';
 import { FiArrowRight } from 'react-icons/fi';
 
 import Layout from '@/components/layout/Layout';
+import Nav from '@/components/nav';
 import Seo from '@/components/Seo';
 
 export default function Account() {
@@ -101,81 +103,90 @@ export default function Account() {
 
       <main>
         <section>
-          <div className='layout wavy flex min-h-screen flex-col justify-center pl-12'>
-            <h1 className='text-4xl font-bold'>{header}</h1>
-            <p className='mb-8 text-lg'>{action}</p>
-            {error ? (
-              <div className='alert alert-error mb-8 w-full max-w-md shadow-lg'>
-                <div>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    className='h-6 w-6 flex-shrink-0 stroke-current'
-                    fill='none'
-                    viewBox='0 0 24 24'
-                  >
-                    <path
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                      strokeWidth='2'
-                      d='M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z'
-                    />
-                  </svg>
-                  <span>{error}</span>
+          <div className='layout wavy flex h-screen flex-col'>
+            <Nav />
+            <div className='flex h-full flex-col justify-center pl-12'>
+              <h1 className='text-4xl font-bold'>{header}</h1>
+              <p className='mb-8 text-lg'>{action}</p>
+              {error ? (
+                <div className='alert alert-error mb-8 w-full max-w-md shadow-lg'>
+                  <div>
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      className='h-6 w-6 flex-shrink-0 stroke-current'
+                      fill='none'
+                      viewBox='0 0 24 24'
+                    >
+                      <path
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        strokeWidth='2'
+                        d='M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z'
+                      />
+                    </svg>
+                    <span>{error}</span>
+                  </div>
                 </div>
-              </div>
-            ) : (
-              <></>
-            )}
+              ) : (
+                <></>
+              )}
 
-            <div className='form-control'>
-              <div className='input-group'>
-                {step === 0 ? (
-                  <input
-                    id='phone-number'
-                    type='tel'
-                    placeholder='Phone Number'
-                    pattern='\+?[0-9]{15}'
-                    maxLength={15}
-                    value={phone}
-                    onChange={(e) => setPhone(e.target.value)}
-                    className='input input-bordered w-full max-w-md'
-                  />
-                ) : step === 1 ? (
-                  <input
-                    id='auth-code'
-                    type='number'
-                    placeholder='Auth Code'
-                    pattern='[0-9]{6}'
-                    maxLength={6}
-                    value={code}
-                    onChange={(e) => setCode(e.target.value)}
-                    className='input input-bordered w-full max-w-md'
-                  />
-                ) : (
-                  <input
-                    id='username'
-                    type='text'
-                    placeholder='Username'
-                    maxLength={25}
-                    pattern='^[a-zA-Z0-9_]*$'
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    className='input input-bordered w-full max-w-md'
-                  />
-                )}
-                <a
-                  className='btn btn-square'
-                  onClick={
-                    step === 0
-                      ? sendReqPhone
-                      : step === 1
-                      ? sendReqCode
-                      : sendReqUsername
-                  }
-                  id='btn-submit'
-                >
-                  <FiArrowRight className='text-lg' id='btn-submit-icon' />
-                </a>
+              <div className='form-control'>
+                <div className='input-group'>
+                  {step === 0 ? (
+                    <input
+                      id='phone-number'
+                      type='tel'
+                      placeholder='Phone Number'
+                      pattern='\+?[0-9]{15}'
+                      maxLength={15}
+                      value={phone}
+                      onChange={(e) => setPhone(e.target.value)}
+                      className='input input-bordered w-full max-w-md'
+                    />
+                  ) : step === 1 ? (
+                    <input
+                      id='auth-code'
+                      type='number'
+                      placeholder='Auth Code'
+                      pattern='[0-9]{6}'
+                      maxLength={6}
+                      value={code}
+                      onChange={(e) => setCode(e.target.value)}
+                      className='input input-bordered w-full max-w-md'
+                    />
+                  ) : (
+                    <input
+                      id='username'
+                      type='text'
+                      placeholder='Username'
+                      maxLength={25}
+                      pattern='^[a-zA-Z0-9_]*$'
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                      className='input input-bordered w-full max-w-md'
+                    />
+                  )}
+                  <a
+                    className='btn btn-square'
+                    onClick={
+                      step === 0
+                        ? sendReqPhone
+                        : step === 1
+                        ? sendReqCode
+                        : sendReqUsername
+                    }
+                    id='btn-submit'
+                  >
+                    <FiArrowRight className='text-lg' id='btn-submit-icon' />
+                  </a>
+                </div>
+                <footer className='absolute bottom-2 w-full text-center'>
+                  made with ❤️ by{' '}
+                  <Link href='/brett' passHref>
+                    <a className='link'>@brett</a>
+                  </Link>
+                </footer>
               </div>
             </div>
           </div>
